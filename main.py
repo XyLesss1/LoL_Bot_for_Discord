a1c4dde19670a6b2a0a7a99d146dd18f81ec5675
XyLesss1 <149181090+XyLesss1@users.noreply.github.com>
2024-08-25 15:23:52 +0500

    удалила

--- main.py
+++ main.py
@@ -27,28 +27,18 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    await member.send('Привет, ты можешь получить всю необходимую информацию по команде !info')
+
     role = discord.utils.get(member.guild.roles, id=1267123714240020511)
     embed = discord.Embed(
-        title="Добро пожаловать на сервер!",
+        title="Welcome!",
         description=f"{member.name}#{member.discriminator}\n"
-                    f"Вы получаете роль - Новичок",
+                    f"You get the role of summoner",
         color=0xffffff
     )
-    for ch in bot.get_guild(member.guild.id).channels:
-        if ch.name == 'общее':
-            await bot.get_channel(ch.id).send(embed=embed)
+    await member.send(embed=embed)
     await member.add_roles(role)
     await db.create_table(db.name_bd)
     await db.create_line_users(member.id)
-
-
-@bot.event
-async def on_member_remove(member):
-    for ch in bot.get_guild(member.guild.id).channels:
-        if ch.name == 'общее':
-            await bot.get_channel(ch.id).send(f'Нам будет не хватать тебя, {member.mention}')
-
 
 @bot.event
 async def on_message(message):
@@ -56,19 +46,7 @@ async def on_message(message):
     for content in message.content.split():
         if content.lower() in bad_words:
             await message.delete()
-            await message.channel.send(f'{message.author.mention}, следи за базаром.')
-
-
-@bot.command()
-async def info(ctx):
-    info = discord.Embed(
-        title='info',
-        description=f'!prof - профиль\n'
-                    f'!stats - статистика'
-    )
-    await ctx.send(embed=info)
-
-
+            await message.channel.send(f'{message.author.mention},')
 @bot.command()
 @commands.has_permissions(kick_members=True, administrator=True)
 async def kick(ctx, member: discord.Member, *, reason="Нарушение правил"):
@@ -76,66 +54,10 @@ async def kick(ctx, member: discord.Member, *, reason="Нарушение пра
     await member.kick(reason=reason)
     await ctx.message.delete()
 
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def ban(ctx, member: discord.Member, *, reason="Многократное нарушение правил"):
-    await ctx.send(f"Admin {ctx.author.mention} забанил пользователя {member.mention}", delete_after=2)
-    await member.ban(reason=reason)
-    await ctx.message.delete()
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def mute(ctx, member: discord.Member, M, *, reason="Нарушение правил"):
-    roles = member.roles
-    role = discord.utils.get(ctx.guild.roles, id=1267405498706169867)
-    for role_mem in roles[1:]:
-        await member.remove_roles(role_mem)
-    await member.add_roles(role, reason=reason)
-    message: discord.Message = await ctx.send(f'User {member.mention} has been muted.')
-    time = M * 60
-    await asyncio.sleep(time)
-    await member.remove_roles(role)
-    for role_mem in roles[1:]:
-        await member.add_roles(role_mem)
-    await ctx.send(f'user {member.mention} has been unmuted.')
-
-
-'''@bot.command()
-@commands.has_permissions(administrator=True)
-async def unban(ctx, id: int, *, reason="Отстуствует"):
-    user = await bot.fetch_user(id)
-    await ctx.guild.unban(user, reason=reason)'''
-
-
-@bot.command()
-async def get_roles(ctx, member: discord.Member):
-    roles = member.roles
-    for role in roles[1:]:
-        await ctx.send(role.id)
-
-
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def clear(ctx, amount=None):
     await ctx.channel.purge(limit=int(amount))
-
-
-'''@bot.command()
-async def get_bans(ctx):
-    banned_users = ctx.guild.bans()
-    async for user in banned_users:
-        print(user)'''
-
-
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def unban(ctx, name, reason='reason'):
-    banned_users = ctx.guild.bans()
-    async for user in banned_users:
-        if user.user.global_name == name:
-            await ctx.guild.unban(user.user)
 
 @bot.command()
 async def stats(ctx, name, tag, region):
@@ -202,22 +124,6 @@ async def prof(ctx):
     await ctx.send(embed=embed)
 
 
-'''@bot.command()
-async def my_activity(ctx):
-    joined = ctx.author.joined_at
-    embed = discord.Embed(
-        title='Activity',
-        color=discord.Colour.dark_blue(),
-    )
-    embed.set_author(
-        name=ctx.author.global_name,
-        icon_url=ctx.author.avatar,
-    )
-    embed.add_field(name='Joined: ', value=joined.date())
-    embed.add_field(name='Кол-во сообщений', value=ctx.author.message_count)
-    embed.add_field(name='Warns: ', value='Warns')
-    embed.set_thumbnail(url=ctx.author.avatar)
-    await ctx.send(embed=embed)'''
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def warn(ctx, member: discord.Member):
